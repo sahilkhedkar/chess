e6a33c916cb3107f84539796b630179c375ebd00
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-12-11 07:41:37 +0530

    created the game class

--- backend1/src/Game.ts
+++ backend1/src/Game.ts
@@ -1,3 +1,17 @@
+import type WebSocket from "ws";
+
 export class Game {
-    
+  private player1: WebSocket;
+  private player2: WebSocket;
+  private moves: string[];
+  private startTime: Date;
+  private board: string;
+
+  constructor(player1: WebSocket, player2: WebSocket) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.moves = [];
+    this.startTime = new Date();
+    this.board = "";
+  }
 }
