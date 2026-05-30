bf31db20146bf34f0cf9cfe985f3fcd46d3f5052
Sahil Khedkar <sahilkhedkar11@gmail.com>
2026-05-30 12:20:53 +0530

    wrote skeleton routes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,28 @@
 import { Elysia } from 'elysia'
 import { node } from '@elysia/node'
+import * as schema from "@repo/shared/zod-schema"
 
-new Elysia({ adapter: node() })
-    .get('/', () => 'Hello Elysia')
-    .listen(3000 , () => {
-        console.log('Server is running on http://localhost:3000')
-    })
+const app = new Elysia({ adapter: node() }).get('/', () => 'Hello Elysia')
+
+
+//signup route
+
+app.post("/signup" , (req) => {
+    console.log(schema.name)
+
+    const { email, password } = req.body
+
+    return `User created successfully`
+})
+
+//login route
+
+app.post("/login" , (req,res) => {
+    const { email, password } = req.body
+
+    return res.status(200).json({ message: 'User logged in successfully' })
+})
+
+app.listen(3000 , () => {
+    console.log('Server is running on http://localhost:3000')
+})
---
+++ packages/shared/src/zod-schema.ts
@@ -0,0 +1,1 @@
+export const name = "zod-schema";
